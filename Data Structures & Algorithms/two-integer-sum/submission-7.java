ab21a97f9ced4cda333b5c449df7ae29ed0b1d49
Alexander Eng <101579893+PaperBagTrick@users.noreply.github.com>
2026-09-01 14:44:06 -0500

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> ansMap = new HashMap<>();
+        int[] ansArr = new int[2];
+        for(int i = 0; i < nums.length; i++) {
+            int dif = target - nums[i];
+            if (ansMap.containsKey(dif)) {
+                return new int[] {ansMap.get(dif), i};
+            }
+            else
+            {
+                ansMap.put(nums[i], i);
+            }
+        }
+        throw new IllegalArgumentException("it doesnt exist");
+    }
+}
